3e7bd7075a207c49117ea2bf0de3c80788f6a70a
latifa <latilwa3id@gmail.com>
2023-05-31 20:23:02 +0200

    enlever les message erreur au reouverture

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -6,7 +6,6 @@ function editNav() {
         x.className = "topnav";
     }
 }
-
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelector(".modal-btn");
@@ -15,16 +14,22 @@ const close = document.querySelector(".close");
 const heroSection = document.querySelector(".hero-section");
 const topnav = document.querySelector(".topnav");
 const footer = document.querySelector("footer");
-const myform = document.getElementById("Myform");
-
+let myform = document.getElementById("Myform");
+let erreur = document.querySelectorAll(".error");
+let erreurP = document.querySelector(".errorP");
+let erreurN = document.querySelector(".errorN");
+let erreurE = document.querySelector(".errorE");
+let erreurD = document.getElementById("errorD");
+let erreurQ = document.getElementById("errorQ");
+let erreurL = document.querySelector(".errorL");
+let accepter = document.querySelector("#accepter");
 // launch modal event
 modalBtn.addEventListener("click", launchModal);
 
 // launch modal form
 function launchModal() {
-    myform.style.display = "block";
+    myform.style.display = "inline-block";
     modalbg.style.display = "block";
-    document.body.style.overflow = "auto";
     heroSection.style.display = "none";
     topnav.style.display = "none";
     footer.style.display = "none";
@@ -36,92 +41,88 @@ function launchModal() {
 close.addEventListener("click", function () {
     modalbg.style.display = "none";
     heroSection.style.display = "block";
-    topnav.style.display = "block";
+    topnav.style.display = "inline-block";
     footer.style.display = "block";
 });
 //la fonction pour apparaitre le message de validation
-function message() {
+function message() {     
+    erreurP.innerHTML = "";
+    erreurN.innerHTML = "";
+    erreurE.innerHTML = "";
+    erreurD.innerHTML = "";
+    erreurQ.innerHTML = "";
+    erreurL.innerHTML = "";
+    accepter.innerHTML = "";
     myform.style.display = "none";
     const modalbody = document.querySelector(".modal-body");
-    modalbody.innerHTML += `<p class="message">Merci pour votre inscription</p><button class="btn-fermer btn-submit">fermer</button>`;
-    //let modalMsg = document.querySelector(".message");
-
-    modalbody.style.height = "50rem";
+    modalbody.innerHTML += `<p class="message">Merci pour votre inscription</p><button class="btn-fermer btn-submit">fermer</button>`;    
+    modalbody.style.height = "48rem";
     //recuperer le boutton pour fermer le modal
     let btnFermer = document.querySelector(".btn-fermer");
-
     //evenement au click sur fermer
     btnFermer.addEventListener("click", function () {
-        let myform = document.getElementById("Myform"); //********************** */
-        myform.style.display = "block";
+        let myform = document.getElementById("Myform");
+        myform.style.display = "block";         
         let modalMsg = document.querySelector(".message");
         btnFermer.remove();
         modalMsg.remove();
         modalbg.style.display = "none";
-
         heroSection.style.display = "block";
-        topnav.style.display = "block";
+        topnav.style.display = "inline-block";
         footer.style.display = "block";
     });
 }
 /*adapter les element du formulaire *******************************/
-
 let validate = false;
 myform.addEventListener("submit", function (event) {
-    event.preventDefault();
-
     //recuperer champ prenom **************************
     let Prenom = document.getElementById("firstname");
     let RegExp = /^[a-zA-Z-\s]+$/;
-    if (Prenom.value == "" && RegExp.test(Prenom.value) == false) {
-        let erreur = document.getElementById("error");
-        erreur.innerHTML = "Veuillez entrer 2 caractères ou plus";
+    if (Prenom.value == "" || RegExp.test(Prenom.value) == false) {
+        erreurP.innerHTML = "Veuillez entrer 2 caractères ou plus";
         validate = false;
+        event.preventDefault();
     } else {
         validate = true;
-        localStorage.setItem("Prenom", Prenom.value);
     }
     //recuperer champ nom***************************************
     let Nom = myform.last;
-    if (Nom.value == "" && RegExp.test(Nom.value) == false) {
-        let erreurN = document.querySelector(".errorN");
+    if (Nom.value == "" || RegExp.test(Nom.value) == false) {
         erreurN.innerHTML = "Veuillez entrer 2 caractères ou plus";
         validate = false;
+        event.preventDefault();
     } else {
         validate = true;
-        localStorage.setItem("inputNom", Nom.value);
     }
-
-    // email*******************************************
-    //recuperer champ email
+     
+    //recuperer champ email***********************************
     let inputEmail = myform.email;
     let RegExpEmail = /^[a-zA-Z0-9._]+[@]{1}[a-zA-Z0-9._]+[.]{1}[a-z]{2,10}$/;
-    if (inputEmail.value == "" && RegExpEmail.test(inputEmail.value) == false) {
-        let erreurE = document.getElementById("errorE");
+    if (inputEmail.value == "" || RegExpEmail.test(inputEmail.value) == false) {
         erreurE.innerHTML = "le champ doit etre au format mail";
         validate = false;
+        event.preventDefault();
     } else {
         validate = true;
-        localStorage.setItem("inputEmail", inputEmail.value);
     }
     //date de naissance**********************************************
     //recuperer champ date de naissance
     let inputDate = myform.birthdate;
     if (inputDate.value == "") {
-        let erreurD = document.getElementById("errorD");
         erreurD.innerHTML = "Vous devez entrer votre date de naissance";
         validate = false;
+        event.preventDefault();
     } else {
         validate = true;
     }
     // verifier le nb de fois******************************************
-    let erreurQ = document.getElementById("errorQ");
 
     //recuperer champ quantite
     let inputQuantity = myform.quantity;
     if (inputQuantity.value == "") {
         erreurQ.innerHTML = "entre un nb entre 0 et 99";
         validate = false;
+        event.preventDefault();
     } else {
         validate = true;
     }
@@ -134,9 +135,9 @@ myform.addEventListener("submit", function (event) {
             break;
         } else {
             console.log("Il faut choisir une location");
-            let erreurL = document.querySelector(".errorL");
             erreurL.innerHTML = "Il faut choisir une location";
             validate = false;
+            event.preventDefault();
         }
     }
     //recuperer la check obligatoire******************************
@@ -144,7 +145,7 @@ myform.addEventListener("submit", function (event) {
         validate = true;
     } else {
         validate = false;
-        let accepter = document.querySelector("#accepter");
+        event.preventDefault();
         accepter.innerHTML = "Vous devez vérifier si vous acceptez les conditions";
     }
     //verifier si tous les champs sont validé
